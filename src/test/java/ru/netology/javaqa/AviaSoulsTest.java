5c64e50d4f0c3b46918969fff36dd2c8dec483cb
Pavel Shvydkin <Pshvydkin@gmail.com>
2023-05-24 00:59:08 +0300

    modified AviaSoulsTest

--- src/test/java/ru/netology/javaqa/AviaSoulsTest.java
+++ src/test/java/ru/netology/javaqa/AviaSoulsTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+
 public class AviaSoulsTest {
     private AviaSouls aviaSouls;
 
@@ -17,7 +19,7 @@ public class AviaSoulsTest {
     }
 
     @Test
-    public void searchByRouteTest() { // В этом тесте мы проверяем работу метода search
+    public void searchByRouteTest() { // Проверяем работу метода search, ищем билеты по маршруту
         Ticket[] expected = {
                 new Ticket("Самара", "Астана", 200, 16, 18)
         };
@@ -26,7 +28,7 @@ public class AviaSoulsTest {
     }
 
     @Test
-    public void searchAndSortByPriceTest() { // проверям метод searchAndSortBy с параметром компаратора по умолчанию ( по возрастанию цены билета
+    public void searchAndSortByPriceTest() { // Проверяем метод searchAndSortBy с параметром компаратора по умолчанию (сортировка по возрастанию цены билета)
         Ticket[] expected = {
                 new Ticket("Самара", "Астана", 200, 16, 18),
         };
@@ -35,7 +37,7 @@ public class AviaSoulsTest {
     }
 
     @Test
-    public void searchAndSortByTimeTest() { // проверям метод searchAndSortBy с сортировкой по времени полета от меньшего к большему
+    public void searchAndSortByTimeTest() { // проверям метод searchAndSortBy с параметром компаратора по времени полета от меньшего к большему
         Ticket[] expected = {
                 new Ticket("Москва", "Санкт-Петербург", 400, 10, 11),
         };
@@ -44,7 +46,7 @@ public class AviaSoulsTest {
     }
 
     @Test
-    public void compareToTest() { // проверяем метод compareTo
+    public void compareToTest() { // Проверяем метод compareTo,  сравниваем цены билетов
         Ticket ticket1 = new Ticket("Москва", "Санкт-Петербург", 400, 10, 11);
         Ticket ticket2 = new Ticket("Екатеринбург", "Казань", 500, 12, 16);
         Ticket ticket3 = new Ticket("Москва", "Воронеж", 400, 20, 23);
@@ -57,5 +59,24 @@ public class AviaSoulsTest {
 
         result = ticket1.compareTo(ticket3);
         Assertions.assertEquals(0, result); // ticket1.price == ticket3.price
+    }
+
+    @Test
+    public void ticketTimeComparatorTest() {  // // Проверяем компаратор TicketTimeComparator
+        Ticket ticket1 = new Ticket("Москва", "Санкт-Петербург", 400, 10, 11);
+        Ticket ticket2 = new Ticket("Сочи", "Челябинск", 300, 14, 17);
+        Ticket ticket3 = new Ticket("Екатеринбург", "Казань", 500, 12, 16);
+        Ticket ticket4 = new Ticket("Москва", "Воронеж", 400, 20, 23);
+
+        Comparator<Ticket> comparator = new TicketTimeComparator();
+
+        int result = comparator.compare(ticket1, ticket2);
+        Assertions.assertTrue(result < 0); // ticket2 < ticket1 по времени полета
+
+        result = comparator.compare(ticket3, ticket2);
+        Assertions.assertTrue(result > 0); // ticket3 > ticket2 по времени полета
+
+        result = comparator.compare(ticket2, ticket4);
+        Assertions.assertEquals(0, result); // ticket2 и ticket4 равны по времени полета
     }
 }
